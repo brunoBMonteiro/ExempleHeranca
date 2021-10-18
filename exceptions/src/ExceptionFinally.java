c4201ce455f04da485af561a696bf5566e05524d
Bruno <brunobilheri@hotmail.com>
2021-10-18 00:18:38 -0300

    Exception finally

---
+++ exceptions/src/ExceptionFinally.java
@@ -0,0 +1,27 @@
+public class ExceptionFinally {
+    public static void main(String[] args){
+
+        // multiplos de 2 e 4
+        int[] numeros = {4, 8, 16 , 32, 64, 128};
+
+        // denominadores
+        int[] denominadores = {2, 0, 4, 8, 0};
+
+        for (int i = 0; i < numeros.length; i++){
+            try {
+                System.out.println(numeros[i] + "/" + denominadores[i] + "=" + (numeros[i] / denominadores[i]));
+            }
+            catch (ArithmeticException e){
+                System.out.println("Erro ao dividir por zero");
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("Posição do array inválida");
+            }
+            finally {
+                System.out.println("Essa linha é impressa sempre após o try ou catch");
+            }
+
+
+        }
+    }
+}
