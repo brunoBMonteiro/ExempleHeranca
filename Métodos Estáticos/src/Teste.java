6434599e98a6d55a6b193f73c3da37b54f7b31ee
Bruno <brunobilheri@hotmail.com>
2021-10-17 23:28:04 -0300

    métodos estaticos

---
+++ Métodos Estáticos/src/Teste.java
@@ -0,0 +1,12 @@
+public class Teste {
+    public static void main(String[] args){
+
+        MinhaCalculadora calculadora = new MinhaCalculadora();
+
+        calculadora.soma(1,2);
+
+
+        // com o static na classe MinhaCalculadora
+        // MinhaCalculadora.soma(1,2);
+    }
+}
